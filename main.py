4a2de9f2a55e38de73ebf074ff0ce74bbce62aa1
Gabriel <142851048+gabreuls@users.noreply.github.com>
2026-04-13 15:33:41 -0300

    Criando quarto commit e um loop

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ print('Commit Inicial')
 print('Criando Segundo commit!')
 
 print('Criando terceiro commit')
+
+print('Criando quarto commit e um loop')
+
+lista = [1,2,3,4,5]
+
+for i in lista:
+  print(i)
